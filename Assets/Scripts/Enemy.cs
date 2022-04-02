079eba098917dc5917e825883a3727e10184a2a1
Tryston Minsquero <tam190005@utdallas.edu>
2022-04-02 13:24:39 -0500

    A star

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using Pathfinding;
 
 public abstract class Enemy : MonoBehaviour
 {
+    public bool isMarked;
+    public float damageAmount;
+    public float attackInterval;
+    
+    
 
-   
+    public abstract void Attack();
+    
+    
+    
+    
+
 }
