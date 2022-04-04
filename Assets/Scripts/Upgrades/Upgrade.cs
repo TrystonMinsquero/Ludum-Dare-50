9f463dc93a6845ae61abbdab096bf2171517eea0
Tryston Minsquero <tam190005@utdallas.edu>
2022-04-04 03:48:48 -0500

    upgrade description

--- Assets/Scripts/Upgrades/Upgrade.cs
+++ Assets/Scripts/Upgrades/Upgrade.cs
@@ -4,6 +4,8 @@
 public abstract class Upgrade
 {
     public string upgradeName;
+    [TextArea(1,3)]
+    public string upgradeDescription;
     public Sprite icon;
     public abstract void ApplyUpgrade();
 }
