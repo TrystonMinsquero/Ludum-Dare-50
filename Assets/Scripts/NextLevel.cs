bb4cb00593077d75c96fef7a70e1e252afb99e5a
Tryston Minsquero <tam190005@utdallas.edu>
2022-04-04 00:54:28 -0500

    updated next level

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
-    public Transform nextLevelCoordinates;
+    public StartRoom nextLevelsStartRoom;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         { 
             Debug.Log("Next Level");
-            col.transform.position = nextLevelCoordinates.position;
+            nextLevelsStartRoom.Spawn(col.transform);
         }
     }
 }
